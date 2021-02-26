b838c6db30baac7cb64b091a5c81a5a93e85fe59
Farhanm312 <farhanmaulana378@gmail.com>
2021-02-26 09:50:27 +0700

    Aktifitas 3 done pak

---
+++ aktifitas_3.py
@@ -0,0 +1,15 @@
+#PERCABANGAN
+print("Percabangan pada Bahasa Pemograman PHYTON")
+print("Pilih bahasa Pemogramaan")
+print("1. phyton")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2) : "))
+
+if pilih == 1:
+    print("bahasa pemograman yang dipilih adalah phyton")
+elif pilih == 2:
+    print("bahasa pemograman yang dipilih adalah C")
+else:
+    print("pilihan anda tidak tersedia!")
+    
