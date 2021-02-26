14b7395070031e59fd510b5c73f28f84cb6f6cf1
Farhanm312 <farhanmaulana378@gmail.com>
2021-02-26 09:24:00 +0700

    Aktifitas 1 done pak

---
+++ aktifitas_1.py
@@ -0,0 +1,12 @@
+#INPUT
+
+nama=input("farhan Maulana")
+umur=19
+
+#OUTPUT
+
+print("")
+print("==INPUT DAN OUTPUT PADA BAHASA PEMOGRAMAN PYTHON==")
+print("Nama :", nama)
+print("Umur :", umur)
+print("")
